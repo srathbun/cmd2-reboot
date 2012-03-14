c54047a29429cd294a104dd9c9189b7bd5115890
Zearin <zearin@gonk.net>
2012-03-14 09:15:33 -0400

    Readability nitpicks to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
 try:
     from setuptools import setup, find_packages
 except ImportError:
@@ -8,55 +9,57 @@ except ImportError:
 import sys
 
 install_requires = ['pyparsing>=1.5.6']
-setup(
-    name="cmd2",
-    version="0.6.4",
-    py_modules=["cmd2"],
-    use_2to3=True,
-    
-    # metadata for upload to PyPI
-    author = 'Catherine Devlin',
-    author_email = 'catherine.devlin@gmail.com',
-    description = "Extra features for standard library's cmd module",
-    license = 'MIT',
-    keywords = 'command prompt console cmd',
-    url = 'http://packages.python.org/cmd2/',
-    install_requires = install_requires,
-    long_description = """Enhancements for standard library's cmd module.
+long_description = '''Enhancements for standard library's ``cmd`` module.
 
-Drop-in replacement adds several features for command-prompt tools:
+This drop-in replacement adds several features for command-prompt tools:
 
     * Searchable command history (commands: "hi", "li", "run")
     * Load commands from file, save to file, edit commands in file
     * Multi-line commands
     * Case-insensitive commands
-    * Special-character shortcut commands (beyond cmd's "@" and "!")
+    * Accepts abbreviated commands when unambiguous
+    * Parse commands with flags
+    * Special-character shortcut commands beyond cmd's ``@`` and ``!``
     * Settable environment parameters
-    * Parsing commands with flags
-    * > (filename), >> (filename) redirect output to file
-    * < (filename) gets input from file
-    * bare >, >>, < redirect to/from paste buffer
-    * accepts abbreviated commands when unambiguous
-    * `py` enters interactive Python console
-    * test apps against sample session transcript (see example/example.py)
+    * ``> _filename_``, ``>> _filename_``: redirect output to _filename_
+    * ``< _filename_``: get input from _filename_
+    * ``>``, ``>>``, ``<`` (without a filename): Redirect to/from the paste buffer
+    * ``py`` enters interactive Python console
+    * Test apps against sample session transcript (see example/example.py)
 
-Useable without modification anywhere cmd is used; simply import cmd2.Cmd in place of cmd.Cmd.
+Usable without modification anywhere ``cmd`` is used. Simply ``import cmd2.Cmd`` in place of ``cmd.Cmd``.
 
 Running `2to3 <http://docs.python.org/library/2to3.html>` against ``cmd2.py`` 
 generates working, Python3-based code.
 
-See docs at http://packages.python.org/cmd2/
-""",
+Documentation:
+http://packages.python.org/cmd2/
+'''
 
-    classifiers=[
+setup(
+    name        =   'cmd2',
+    version     =   '0.6.5',
+    py_modules  =   ['cmd2'],
+    use_2to3    =   True,
+    
+    # metadata for upload to PyPI
+    author              =   'Catherine Devlin',
+    author_email        =   'catherine.devlin@gmail.com',
+    description         =   '''Extra features for standard library's cmd module''',
+    license             =   'MIT',
+    keywords            =   'command prompt console cmd shell cli',
+    url                 =   'http://packages.python.org/cmd2/',
+    install_requires    =   install_requires,
+    long_description    =   long_description,
+    classifiers         =   [
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'Programming Language :: Python',
+        'Programming Language :: Python 3',
         'License :: OSI Approved :: MIT License',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
+        'Topic :: Software Development :: Libraries :: Python Modules'],
     )
 
