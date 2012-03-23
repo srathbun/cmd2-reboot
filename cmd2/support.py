0868e1ba5fd368b15c88b5ba304b317e9e47afda
Zearin <zearin@gonk.net>
2012-03-23 10:20:13 -0400

    Moved platform-dependent pastebuffer codeblock into `support` module.

--- cmd2/support.py
+++ cmd2/support.py
@@ -18,7 +18,12 @@ import  six
 
 #   Standard Library Imports
 #   --------------------------------------------------------
-import  collections, re
+import  collections,    \
+        copy,           \
+        os,             \
+        re,             \
+        subprocess,     \
+        sys
 
 
 #   Third Party Imports
@@ -27,8 +32,136 @@ import  collections, re
 
 #   Cmd2 Modules
 #   --------------------------------------------------------
-from    .parsers    import (OptionParser,
-                            ParsedString)
+from    .errors     import  PASTEBUFF_ERR
+
+__all__ =   [   'HistoryItem',
+                'History',
+                'Statekeeper',
+                'stubbornDict',
+                'can_clip',
+                'cast',
+                'ljust',
+                'get_paste_buffer',
+                'replace_with_file_contents',
+                'write_to_paste_buffer'         ]             
+
+
+
+if subprocess.mswindows:    
+    '''
+    Initializes the methods `get_paste_buffer` and `write_to_paste_buffer`
+    appropriately for the platform.
+    '''
+    
+    try:
+        import win32clipboard
+        def get_paste_buffer():
+            win32clipboard.OpenClipboard(0)
+            try:
+                result = win32clipboard.GetClipboardData()
+            except TypeError:
+                result = ''  #non-text
+            win32clipboard.CloseClipboard()
+            return result            
+        def write_to_paste_buffer(txt):
+            win32clipboard.OpenClipboard(0)
+            win32clipboard.EmptyClipboard()
+            win32clipboard.SetClipboardText(txt)
+            win32clipboard.CloseClipboard()        
+    except ImportError:
+        def get_paste_buffer(*args):
+            raise OSError, PASTEBUFF_ERR % ('pywin32', 'Download from http://sourceforge.net/projects/pywin32/')
+        write_to_paste_buffer = get_paste_buffer
+elif sys.platform == 'darwin':
+    can_clip = False
+    try:
+        # test for pbcopy. (Should always be installed on OS X, AFAIK.)
+        subprocess.check_call(  'pbcopy -help', 
+                                shell   = True, 
+                                stdout  = subprocess.PIPE, 
+                                stdin   = subprocess.PIPE, 
+                                stderr  = subprocess.PIPE)
+        can_clip = True
+    except (subprocess.CalledProcessError, OSError, IOError):
+        #   @FIXME
+        #       Under what circumstances might this be raised?
+        pass
+    if can_clip:
+        def get_paste_buffer():
+            pbcopyproc = subprocess.Popen(  'pbcopy -help', 
+                                            shell   =True, 
+                                            stdout  =subprocess.PIPE, 
+                                            stdin   =subprocess.PIPE, 
+                                            stderr  =subprocess.PIPE)
+            return pbcopyproc.stdout.read()
+        def write_to_paste_buffer(txt):
+            pbcopyproc = subprocess.Popen(  'pbcopy', 
+                                            shell   =True, 
+                                            stdout  =subprocess.PIPE, 
+                                            stdin   =subprocess.PIPE, 
+                                            stderr  =subprocess.PIPE)
+            pbcopyproc.communicate(txt.encode())
+    else:
+        def get_paste_buffer(*args):
+            raise OSError, PASTEBUFF_ERR % ('pbcopy', 'Error should not occur on OS X; part of the default installation')
+        write_to_paste_buffer = get_paste_buffer
+else:
+    can_clip = False
+    try:
+        subprocess.check_call(  'xclip -o -sel clip', 
+                                            shell   = True, 
+                                            stdout  = subprocess.PIPE, 
+                                            stdin   = subprocess.PIPE, 
+                                            stderr  = subprocess.PIPE)
+        can_clip = True
+    except AttributeError:  # check_call not defined, Python < 2.5
+        try:
+            test_str  = 'Testing for presence of xclip.'
+            xclipproc   = subprocess.Popen( 'xclip -sel clip', 
+                                            shell   = True, 
+                                            stdout  = subprocess.PIPE, 
+                                            stdin   = subprocess.PIPE)
+            xclipproc.stdin.write(test_str)
+            xclipproc.stdin.close()
+            xclipproc   = subprocess.Popen( 'xclip -o -sel clip', 
+                                            shell   = True, 
+                                            stdout  = subprocess.PIPE, 
+                                            stdin   = subprocess.PIPE)        
+            if xclipproc.stdout.read() == test_str:
+                can_clip = True
+        except Exception: 
+            #   Hate a bare Exception call, but exception 
+            #   classes vary too much b/t stdlib versions
+            pass
+    except Exception:
+        #   Something went wrong with xclip and we cannot use it
+        pass 
+    if can_clip:    
+        def get_paste_buffer():
+            xclipproc = subprocess.Popen(   'xclip -o -sel clip', 
+                                            shell   = True, 
+                                            stdout  = subprocess.PIPE, 
+                                            stdin   = subprocess.PIPE)
+            return xclipproc.stdout.read()
+        def write_to_paste_buffer(txt):
+            xclipproc = subprocess.Popen(   'xclip -sel clip', 
+                                            shell   = True, 
+                                            stdout  = subprocess.PIPE, 
+                                            stdin   = subprocess.PIPE)
+            xclipproc.stdin.write(txt.encode())
+            xclipproc.stdin.close()
+            # but we want it in both the "primary" and "mouse" clipboards
+            xclipproc = subprocess.Popen(   'xclip', 
+                                            shell   = True, 
+                                            stdout  = subprocess.PIPE, 
+                                            stdin   = subprocess.PIPE)
+            xclipproc.stdin.write(txt.encode())
+            xclipproc.stdin.close()
+    else:
+        def get_paste_buffer(*args):
+            raise OSError, PASTEBUFF_ERR % ('xclip', 'On Debian/Ubuntu, install with "sudo apt-get install xclip"')
+        write_to_paste_buffer = get_paste_buffer
+
 
 
 class HistoryItem(str):
